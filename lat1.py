097208c4a974c569e08145d74713320a15b790f6
ZahwaCantik <khoerinazahwa@gmail.com>
2024-02-10 18:52:43 +0900

    v1

---
+++ lat1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+st.write("Coding Club 2024")
+df = pd.DataFrame({
+    'No': [1,2,3,4],
+    'Names': ['Angga','Nadlip','Zahwa','Nadia'],
+    'Nilai': [80,23,99,8]
+})
+
+df
