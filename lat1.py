9022ca54c81a7cb4a5ee347970e099156eb5f7db
ZahwaCantik <khoerinazahwa@gmail.com>
2024-02-10 19:13:13 +0900

    v3

--- lat1.py
+++ lat1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 st.write("Coding Club 2024")
 df = pd.DataFrame({
     'No': [1,2,3,4],
