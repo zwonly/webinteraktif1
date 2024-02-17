fc1ccdf65c4d9cd6f6c455f47a7ce2ec8fd9a05a
ZahwaCantik <khoerinazahwa@gmail.com>
2024-02-17 18:14:00 +0900

    rev

--- lat1.py
+++ lat1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
-st.write("Coding Club 2024")
+st.write("Coding Club 2024 Rev")
 df = pd.DataFrame({
     'No': [1,2,3,4],
     'Names': ['Angga','Nadlip','Zahwa','Nadia'],
