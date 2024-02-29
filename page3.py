ae89b789853735f62e67b037e58ad0b42b57c9a2
ZahwaCantik <khoerinazahwa@gmail.com>
2024-02-29 17:26:13 +0900

    rv6

--- page3.py
+++ page3.py
@@ -2,7 +2,8 @@ import streamlit as st
 def page_3():
     st.title("Halaman 3")
     st.write('Halaman ini Menampilkan Rumus Matematika Turunan Fungsi')
-    
+    st.write('Setelah nonton kartun, yuk belajar matematika bareng. Matematika ilmu yang menyenangkan bukan? Jangan lupa ditonton yaa^^')
+    st.video('https://youtu.be/BfrBmOqN-Aw?si=uiCHtTZZsowz0YhK')
     with open('Untitled61 (1).md','r') as file:
         isi_teks = file.read()
     st.markdown(isi_teks)
