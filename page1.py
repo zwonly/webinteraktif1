9d89b9b425d5da75fa41d6817a12a74206d57d3b
ZahwaCantik <khoerinazahwa@gmail.com>
2024-02-29 17:31:16 +0900

    rv7

--- page1.py
+++ page1.py
@@ -3,4 +3,4 @@ def page_1():
     st.title("Home")
     st.write('Halaman ini akan menjelaskan tentang kartun.')
     st.write('Kartun adalah jenis seni rupa yang biasanya digambar, sering kali animasi, dalam gaya tidak realistis atau semi-realistis. Arti khusus telah berkembang dari waktu ke waktu, tetapi penggunaan modern biasanya mengacu pada gambar atau rangkaian gambar yang ditujukan untuk sindiran, karikatur, atau humor atau gambar bergerak yang mengandalkan rangkaian ilustrasi untuk animasinya. Seseorang yang membuat kartun dalam pengertian pertama disebut kartunis dan dalam pengertian kedua biasanya disebut pembuat animasi.')
-    st.write('Hati-hati untuk membuka page 3. Ada kejutan ^.^')
+    st.write('Hati-hati untuk membuka page 3. Ada kejutan yang sangat menegangkan^.^')
